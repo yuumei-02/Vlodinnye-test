717f4df715c88ec850c382e34c42b2bf4857d23b
Yuumei <shjesna@protonmail.com>
2026-05-17 15:39:06 +0200

    install command

--- vmake.c
+++ vmake.c
@@ -3,16 +3,21 @@
 
 Vmake vmake;
 
-bool build() {
+i32 build() {
    ModuleId mod_1 = Module_new("vtest", "./vtest", MT_Executable);
    return Vmake_build(mod_1, BuildOptions_default_debug());
+}
+
+i32 install() {
+   return execute_command("sudo cp ./vtest/vtest.h /usr/local/include/vtest.h");
 }
 
 i32 main(i32 argc, cstr argv[]) {
    vmake = Vmake_go_rebuild_yourself(argc, argv);
 
    cstr_match(argv[1]) {
-      ncstreq("build") return build();
+      ncstreq("build")  return build();
+      cstreq("install") return install();
       else {
          eprintln("[!] Unknown command \"%s\"", argv[1]);
          return 1;
